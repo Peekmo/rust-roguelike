7b0808ada2f1df20a3dc82cfca7b398fb9f17615
Peekmo <axel.anceau@gmail.com>
2015-01-23 20:55:10 +0100

    Cleaning code

--- src/main.rs
+++ src/main.rs
@@ -2,19 +2,18 @@ extern crate tcod;
 use tcod::{Console, BackgroundFlag, KeyCode, Key};
 
 fn main() {
+    let mut con = Console::init_root(80, 50, "Roguelike", false);
+    let mut exit = false;
 
-    println!("Hello, world!");
+    while !(Console::window_closed() || exit) {
+        con.clear();
+        con.put_char(40, 25, '@', BackgroundFlag::Set);
+        Console::flush();
 
-    let mut con = Console::init_root(80, 50, "libtcod Rust tutorial", false);
-        let mut exit = false;
-            while !(Console::window_closed() || exit) {
-                    con.clear();
-                    con.put_char(40, 25, '@', BackgroundFlag::Set);
-                    Console::flush();
-                    let keypress = Console::wait_for_keypress(true);
-                    match keypress.key {
-                                Key::Special(KeyCode::Escape) => exit = true,
-                                _ => {}
-                            }
-                }
+        let keypress = Console::wait_for_keypress(true);
+        match keypress.key {
+            Key::Special(KeyCode::Escape) => exit = true,
+            _ => {}
+        }
+    }
 }
